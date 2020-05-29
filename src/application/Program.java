b045934eae9664b057cdb9f51bb2a177a247d444
Ederson Ferreira <ederson.dev@gmail.com>
2020-05-29 01:00:41 -0300

    exemplo de delete usando jdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -13,16 +14,15 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			String sql = "UPDATE seller set BaseSalary = BaseSalary + ? WHERE DepartmentId = ?";
+			String sql = "DELETE FROM department WHERE Id = ?";
 			st = conn.prepareStatement(sql);
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 5);
 			
 			int rowsAffected = st.executeUpdate();
 			System.out.println("Done! Rows affected: " + rowsAffected);
 			
 		} catch(SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
