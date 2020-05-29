6fb1b1cea8d7c5af1bdb3383199613d164e1a872
Ederson Ferreira <ederson.dev@gmail.com>
2020-05-29 00:10:19 -0300

    exemplo de insert usando jdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,48 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from department");
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			String sql = "INSERT INTO seller (Name,Email,BirthDate,BaseSalary,DepartmentId) VALUES (?,?,?,?,?)";
+			st = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			String msg = "No rows affected!";
+			if( rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					msg = "Done! Id = " + id;
+				}
 			}
-		} catch (SQLException e) {
+			System.out.println(msg);
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(ParseException e) {
 			e.printStackTrace();
 		} finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
